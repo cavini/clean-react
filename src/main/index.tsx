2c9b47378f29260be3a59ef1a81b6a7aaf1a3d7e
Arthur Cavini <arthurcavini@hotmail.com>
2021-09-24 12:51:34 -0300

    feature: add login view

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import Login from '@/presentation/pages/login/login'
 
 ReactDOM.render(
-    <Login />,
-    document.getElementById('main')
+  <Login />,
+  document.getElementById('main')
 )
