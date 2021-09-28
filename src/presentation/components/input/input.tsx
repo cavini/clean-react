e7f64ce1355063f8dd7b738c897452e3242faabe
Arthur Cavini <arthurcavini@hotmail.com>
2021-09-28 17:18:15 -0300

    feature: ensure Login calls validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,8 +5,14 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
+  const handleChange = (e: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
   const getStatus = (): string => {
     return '🔴'
   }
@@ -15,7 +21,7 @@ const Input: React.FC<Props> = (props: Props) => {
   }
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input data-testid={props.name} {...props} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
 
