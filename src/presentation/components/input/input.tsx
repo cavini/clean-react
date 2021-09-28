58cd6ef96e8730bb5840b08bac60dbbee66157e2
Arthur Cavini <arthurcavini@hotmail.com>
2021-09-28 14:57:50 -0300

    fix: move global styles to index

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -6,7 +6,6 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 const Input: React.FC<Props> = (props: Props) => {
   return (
     <div className={Styles.inputWrap}>
-
       <input {...props} />
       <span className={Styles.status}>🔴</span>
     </div>
