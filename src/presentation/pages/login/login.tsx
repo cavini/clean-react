43c57c1ba91f3e2c009182cc7333b372e5fc2a31
Arthur Cavini <arthurcavini@hotmail.com>
2021-09-29 12:14:25 -0300

    feature: ensure Login presents a spinner on submit

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -27,11 +27,16 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     })
   }, [state.email, state.password])
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    setState({ ...state, isLoading: true })
+  }
+
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form} action="">
+        <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite o seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
